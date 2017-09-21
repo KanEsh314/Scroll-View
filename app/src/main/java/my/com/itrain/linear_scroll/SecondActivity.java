81c3b5fe2818b78e2d72cd01685c1063172361be
KanEsh314 <kanesh314@gmail.com>
2017-09-21 16:53:29 +0800

    Second Commit

---
+++ app/src/main/java/my/com/itrain/linear_scroll/SecondActivity.java
@@ -0,0 +1,13 @@
+package my.com.itrain.linear_scroll;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
